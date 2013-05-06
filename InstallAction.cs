0f945274340582205a8cfc1050ee18ded985c7f4
Marco Wiedemeyer <mail@mwiedemeyer.de>
2013-05-06 11:04:44 +0200

    initial commit

---
+++ InstallAction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using Microsoft.Win32;
+using System.Security.Permissions;
+
+
+namespace BrowserSelector
+{
+    [RunInstaller(true)]
+    public partial class InstallAction : Installer
+    {
+        public InstallAction()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            string command = "\"" + this.Context.Parameters["assemblypath"] + "\" \"%1\"";
+            string icon = "\"" + this.Context.Parameters["assemblypath"] + "\",1";
+
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\http\UserChoice", "Progid", "BrowserSelector.Protocol", RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\Shell\Associations\UrlAssociations\https\UserChoice", "Progid", "BrowserSelector.Protocol", RegistryValueKind.String);
+            
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\BrowserSelector.Protocol", "FriendlyTypeName", "BrowserSelector Protocol", RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\BrowserSelector.Protocol", "URL Protocol", "", RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\BrowserSelector.Protocol", "EditFlags", 2, RegistryValueKind.DWord);
+
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\BrowserSelector.Protocol\DefaultIcon", "", icon, RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\BrowserSelector.Protocol\shell\open\command", "", command, RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\BrowserSelector.Protocol\shell\open\ddeexec\Application", "", "Browser Selector", RegistryValueKind.String);
+            Registry.SetValue(@"HKEY_CLASSES_ROOT\BrowserSelector.Protocol\shell\open\ddeexec\Topic", "", "WWW_OpenURL", RegistryValueKind.String);
+        }
+    }
+}
---
+++ RuleManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace BrowserSelector
+{
+    internal static class RuleManager
+    {
+        internal static string GetExecutable(string url, out string mappedUrl)
+        {
+            mappedUrl = url;
+
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Browser Selector\\Config.xml"));
+
+                var ruleNodes = doc.DocumentElement.SelectNodes("//Rules/Rule");
+
+                var browserRule = (from XmlNode n in doc.DocumentElement.SelectNodes("//Rules/Rule")
+                                   where Regex.IsMatch(url.ToUpper(), n.Attributes["Url"].Value.ToUpper())
+                                   select n.Attributes["Browser"].Value).FirstOrDefault();
+
+                var mappingRule = (from XmlNode n in doc.DocumentElement.SelectNodes("//Rules/Rule")
+                                   where Regex.IsMatch(url.ToUpper(), n.Attributes["Url"].Value.ToUpper()) &&
+                                            n.Attributes["MapsTo"] != null
+                                   select new { Pattern = n.Attributes["Url"].Value, Mapping = n.Attributes["MapsTo"].Value }).FirstOrDefault();
+
+                if (mappingRule != null && !String.IsNullOrEmpty(mappingRule.Mapping))
+                {
+                    mappedUrl = Regex.Replace(url, mappingRule.Pattern, mappingRule.Mapping);
+                }
+
+                string executable = string.Empty;
+
+                if (String.IsNullOrEmpty(browserRule))
+                {
+                    executable = (from XmlNode n in doc.DocumentElement.SelectNodes("//Browsers/Browser")
+                                  where (n.Attributes["Default"] != null &&
+                                    bool.Parse(n.Attributes["Default"].Value))
+                                  select n.Attributes["Path"].Value).FirstOrDefault();
+                }
+                else
+                {
+                    executable = (from XmlNode n in doc.DocumentElement.SelectNodes("//Browsers/Browser")
+                                  where n.Attributes["Name"].Value == browserRule
+                                  select n.Attributes["Path"].Value).FirstOrDefault();
+                }
+
+                return executable;
+            }
+            catch { return null; }
+        }
+    }
+}
